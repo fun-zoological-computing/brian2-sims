2397754ac76e874eb66bede6f8ec8d5d046f18d0
Russell Jarvis <russelljarvis@protonmail.com>
2023-02-24 10:49:58 +1100

    Update UMAP_of_spikes.jl

--- julia_read_dir/UMAP_of_spikes.jl
+++ julia_read_dir/UMAP_of_spikes.jl
@@ -2,3 +2,14 @@
 using UMAP
 using Conda
 using PyCall
+
+py"""
+def send_to_Julia_namespace():
+  import pickle
+  spikes_list = pickle.load("spikes_for_julia_read.p","rb")
+  return (spikes_list[0),spikes_list[1])
+"""
+
+spikes_list0,spikes_list1 = py"send_to_Julia_namespace"()
+@show(spikes_list0)
+@show(spikes_list1)
