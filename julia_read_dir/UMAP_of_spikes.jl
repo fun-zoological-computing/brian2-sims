9371186fedfd8d304b3ade08dd0fc8b7a215b8b6
Russell Jarvis <russelljarvis@protonmail.com>
2023-02-24 11:31:19 +1100

    Update UMAP_of_spikes.jl

--- julia_read_dir/UMAP_of_spikes.jl
+++ julia_read_dir/UMAP_of_spikes.jl
@@ -7,9 +7,9 @@ py"""
 def send_to_Julia_namespace():
   import pickle
   spikes_list = pickle.load("spikes_for_julia_read.p","rb")
-  return (spikes_list[0),spikes_list[1])
+  return (spikes_list[0],spikes_list[1])
 """
 
-spikes_list0,spikes_list1 = py"send_to_Julia_namespace"()
+(spikes_list0,spikes_list1) = py"send_to_Julia_namespace"()
 @show(spikes_list0)
 @show(spikes_list1)
